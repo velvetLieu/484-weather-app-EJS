a2066e2768afba05364a0f5ad39b0b80961f1ec4
brandon <brandondupitas@gmail.com>
2020-12-08 20:29:43 -0800

    configured 5day forecast

--- server.js
+++ server.js
@@ -55,10 +55,13 @@ app.post('/zipcode', function (req, res) {
     data = response.data;
     const response2 = await axios.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&exclude=minutely,hourly&appid=b0de12ed03277da2744c6b4d4a8e3c8f&units=imperial");
     daily = response2.data;
-    console.log(daily.daily[0].dt);
-    console.log(new Date(daily.daily[0].dt*1000));
+    let today = daily.current;
+    let fiveDay = daily.daily;
     var description;
+     
 
+    console.log(fiveDay[1]);
+    
     // checks null alert values
     if(!daily.alerts){
       description = "Weather Description Not Available"
@@ -66,13 +69,19 @@ app.post('/zipcode', function (req, res) {
     else{
       description = daily.alerts[0].description
     }
+
+
     res.render(__dirname + "/views/result", {
       City: data.name,
       Zipcode: zipCode,
       Description: description,
       longitude: data.coord.lon,
       latitude: data.coord.lat,
-      date: new Date()
+      date: new Date(),
+
+      Today: today,
+      //Array 
+      Daily: fiveDay
     })
   }
 
