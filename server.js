8cea073cb23eeb3a56621284cf13497de7066cea
Jonathan Lieu <jonathan.lieu.57@my.csun.edu>
2020-12-11 02:49:41 -0800

    Revert "ignore"
    This reverts commit 71676bcc9f09746ad80e96b60f5b4beaf3840730.

--- server.js
+++ server.js
@@ -1,42 +1,70 @@
-@ -14,6 +14,7 @@ const axios = require('axios');
+// Express server to create a back-end to our web application
+const express = require('express');
+const app = express();
+//Port configuration for Heroku
+const port = process.env.PORT || 3000;
+//NPM package bodyParser , to convert API call data into JSON object
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+const axios = require('axios');
+
 
 
 let ejs = require('ejs');
-const { response } = require('express');
 app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
-@ -25,9 +26,56 @@ app.get('/', function (req, res) {
+
+
+app.get('/', function (req, res) {
+  res.render(__dirname + "/views/index");
+});
 
 
 app.get('/zipcode', function (req, res) {
   res.render(__dirname + "/views/zipcode");
-  res.render(__dirname + "/views/index");
 });
 
-app.get('/city',function (req, res) {
-  res.render(__dirname + "/views/index");
-});
-
-app.post('/city', function(req,res){
-  var cityName = req.body.City;
-  var url1 = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=b0de12ed03277da2744c6b4d4a8e3c8f";
 
 
-  const getCity = async function(url1){
-    const response = await axios.get(url1);
+app.post('/zipcode', function (req, res) {
+
+  var zipCode = req.body.zipcode;
+  var data;
+  var daily;
+
+  // // appends 0's to the zipcodes that have leading 00, 
+  // // 00501 is the smallest but valid zip code 
+
+  while (zipCode.length < 5) {
+    zipCode = "0" + zipCode;
+  };
+
+  var url = "https://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + ",us&appid=b0de12ed03277da2744c6b4d4a8e3c8f"
+  // makes two api calls, using async and await this 
+
+  
+  const getWeather = async function (url) {
+   
+    const response = await axios.get(url);
     data = response.data;
     const response2 = await axios.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&exclude=minutely,hourly&appid=b0de12ed03277da2744c6b4d4a8e3c8f&units=imperial");
     daily = response2.data;
     const response3 = await axios.get("http://api.openweathermap.org/data/2.5/air_pollution?lat="+data.coord.lat+"&lon="+data.coord.lon+"&appid=b0de12ed03277da2744c6b4d4a8e3c8f");
     airQuality = response3.data;
 
+   console.log(daily);
+
+
     let today = daily.current;
     let fiveDay = daily.daily;
     let airQualityIndex = airQuality.list[0].main.aqi;
     var description;
-
-
+     
+    
     // checks null alert values
     if(!daily.alerts){
       description = "Weather Description Not Available"
@@ -48,19 +76,30 @@ app.post('/city', function(req,res){
 
     res.render(__dirname + "/views/result", {
       City: data.name,
+      Zipcode: zipCode,
       Description: description,
       longitude: data.coord.lon,
       latitude: data.coord.lat,
       date: new Date(),
-      airQualityIndex:airQualityIndex,
+      airQualityIndex:airQualityIndex, 
       Today: today,
-      //Array
+      //Array 
       Daily: fiveDay
     })
-
   }
-  getCity(url1);
+
+
+  getWeather(url);
+
 });
 
 
-app.post('/zipcode', function (req, res) {
+
+function logData(data) {
+  console.log(data);
+}
+
+
+app.listen(port, function (req, res) {
+  console.log("bee boop, weather service initialized");
+});
